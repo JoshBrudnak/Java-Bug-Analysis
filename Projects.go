0887a23356546ea4c89320b2da4ce213b8267b2c
Josh31415 <jobrud314@gmail.com>
2017-10-08 23:45:02 -0400

    Added webpage scraper for maven group ids

--- Projects.go
+++ Projects.go
@@ -2,9 +2,40 @@ package main
 
 import (
   "fmt"
+  "strings"
   "os/exec"
-  "sync"
+  "net/http"
+  "golang.org/x/net/html"
 )
+
+func getProjectList(url string) []string {
+  var list []string
+  page, _ := http.Get(url)
+  tokenizer := html.NewTokenizer(page.Body)
+
+  for {
+    token := tokenizer.Next()
+
+    if token == html.ErrorToken {
+      break
+    }
+    t := tokenizer.Token()
+
+    if t.Data == "a" {
+      for _, a := range t.Attr {
+        xml := strings.Contains(string(a.Key), "xml")
+        txt := strings.Contains(string(a.Key), "txt")
+        if a.Key == "href" && !xml && !txt {
+          list = append(list, strings.Trim(a.Val, "/"))
+        }
+      }
+    }
+  }
+
+  page.Body.Close()
+
+  return list
+}
 
 func downloadProject(project string, finished chan bool) {
   cmdUrl := "-DrepoUrl=\"http://repo1.maven.org/maven2\""
@@ -18,11 +49,10 @@ func downloadProject(project string, finished chan bool) {
 }
 
 func main() {
-  projects := []string{"abbot", "acegisecurity", "activation", "activecluster", "activeio", "activemq"}
+  projects := getProjectList("http://repo1.maven.org/maven2")
 
   complete := make([]chan bool, len(projects))
-  for i := range projects {
-    wg.Add(1)
+  for i := 0; i < 10; i++ {
     go downloadProject(projects[i], complete[i])
   }
 
