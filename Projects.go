a92db46bd1193e0226e286b6014750f93d237d55
Josh31415 <jobrud314@gmail.com>
2017-10-12 19:17:08 +0000

    Added project metadata parsing

--- Projects.go
+++ Projects.go
@@ -6,11 +6,25 @@ import (
   "os/exec"
   "net/http"
   "golang.org/x/net/html"
+  "io/ioutil"
+  "encoding/xml"
 )
+
+type Query struct {
+	GroupId string `xml:"groupId"`
+	ArtifactId string `xml:"artifactId"`
+	Latest string `xml:"versioning>latest"`
+	Release string `xml:"versioning>release"`
+	Versions []string `xml:"versioning>versions>version"`
+}
 
 type group struct {
   groupId string
   artifacts []string
+}
+
+func parsePomFiles() {
+
 }
 
 func getGroupIds(url string) []string {
@@ -52,7 +66,31 @@ func getProjectList(baseUrl string) []group {
   return groupList
 }
 
-func downloadProject(repoUrl string, project group, finished chan bool) {
+func getMetaData(group string, artifact string, url string) {
+    var metaData Query
+
+    cmdUrl := url + string(group) + "/" + artifact + "/maven-metadata.xml"
+    fmt.Println(cmdUrl)
+    page,_ := http.Get(cmdUrl)
+    if page.StatusCode == 200 {
+      data,_ := ioutil.ReadAll(page.Body)
+      xml.Unmarshal(data, &metaData)
+    } else {
+      versions := getGroupIds(cmdUrl)
+      latest := versions[len(versions) - 1]
+      metaData = metaData{group, artifact, latest, latest, versions}
+    }
+
+    return metaData
+}
+
+func downloadProject(repoUrl string, project group) {
+  for _,artifact := range project.artifacts {
+    metaData := getMetaData(project.groupId, artifact, repoUrl)
+    url := repoUrl + string(project.groupId) + "/" + artifact + "/" + mataData.Latest
+}
+
+func mvnDownloadProject(repoUrl string, project group, finished chan bool) {
   cmdUrl := "-DrepoUrl=\"" + repoUrl + "\""
   for i := range project.artifacts {
     artifact := "-Dartifact=" + project.groupId + ":" + project.artifacts[i] + ":LATEST"
@@ -64,16 +102,18 @@ func downloadProject(repoUrl string, project group, finished chan bool) {
 }
 
 func main() {
-  batchLength := 10
-  batchNum := 10
+  //batchLength := 10
+  //batchNum := 10
   repository := "http://repo1.maven.org/maven2/"
   projects := getProjectList(repository)
 
+  downloadProject(repository, projects[1])
+  /*
   for i := 0; i < batchNum; i++ {
     complete := make([]chan bool, batchLength)
     for j := 0; j < batchLength; j++ {
       complete[j] = make(chan bool, 1)
-      go downloadProject(repository, projects[(i + 1) * j], complete[(i + 1) * j])
+      go mvnDownloadProject(repository, projects[(i + 1) * j], complete[(i + 1) * j])
     }
 
     for i := 0; i < batchLength; i++ {
@@ -82,4 +122,5 @@ func main() {
     }
   }
   fmt.Println("Download completed")
+  */
 }
